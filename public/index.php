f3483ce46bf7be21375c7c5197739eacf93b4e7f
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-14 22:02:49 +0200

    Update index.php

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Página Principal</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <h1>Bienvenido al proyecto PDFCalendario</h1>
+    <!-- Aquí podrías incluir un menú o enlaces a las otras funcionalidades -->
+</body>
+</html>
